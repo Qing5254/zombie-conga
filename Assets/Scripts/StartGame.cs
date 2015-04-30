145c46b4ec9e92b1a6e0b5185d47e01b5a7b1b10
Qing5254 <yangqing5254@gmail.com>
2015-04-30 19:42:11 +1000

    zombie-conga
    # =============== #
    # Unity generated #
    # =============== #
    Temp/
    Obj/
    UnityGenerated/
    Library/
    Build/

---
+++ Assets/Scripts/StartGame.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class StartGame : MonoBehaviour {
+
+
+	// Use this for initialization
+	void Start () {
+		Invoke("LoadLevel", 3f);
+	}
+
+	public void LoadLevel() {
+		Application.LoadLevel("CongaScene");
+	}
+}
